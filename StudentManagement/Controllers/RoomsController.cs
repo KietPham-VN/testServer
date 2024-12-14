451c462df3afba9d53a2064a6780bcfa87741109
KietPham-VN <phamanhkiet.dev@gmail.com>
2024-12-14 11:06:05 +0700

    first commit

---
+++ StudentManagement/Controllers/RoomsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Entities;
+
+namespace StudentManagement.Controllers
+{
+    [Route("api/rooms")]
+    [ApiController]
+    public class RoomsController(StudentManagementContext studentManagementContext) : ControllerBase
+    {
+        private readonly StudentManagementContext _studentManagementContext = studentManagementContext;
+
+        [HttpGet]
+        [Route("hello")]
+
+        public IActionResult HelloWorld()
+        {
+            return Ok("Hello World");
+        }
+
+        [HttpGet]
+        public IActionResult GetRooms()
+        {
+            var rooms = _studentManagementContext.Rooms.ToList();
+            return Ok(rooms);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetRooms([FromRoute] Guid id)
+        {
+            var rooms = _studentManagementContext.Rooms.Where(x => x.Id == id).FirstOrDefault();
+            if (rooms == null)
+            {
+                return NotFound("Khong tim thay phong");
+            }
+            return Ok(rooms);
+        }
+
+        [HttpPost]
+        public IActionResult CreateRooms([FromBody] Room room)
+        {
+            _studentManagementContext.Add(room);
+            var result = _studentManagementContext.SaveChanges();
+            if (result > 0)
+            {
+                return StatusCode(201, result);
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteRooms([FromRoute] Guid id)
+        {
+            var room = _studentManagementContext.Rooms.Where(x => x.Id == id).FirstOrDefault();
+            if (room == null)
+            {
+                return NotFound();
+            }
+            _studentManagementContext.Remove(room);
+            var result = _studentManagementContext.SaveChanges();
+            if (result > 0)
+            {
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+        [HttpPatch]
+        [Route("{id}")]
+        public IActionResult UpdateRooms([FromRoute] Guid id, [FromBody] Room updatedRoom)
+        {
+            var room = _studentManagementContext.Rooms.Where(x => x.Id == id).FirstOrDefault();
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            room.Name = updatedRoom.Name;
+            _studentManagementContext.Update(room);
+            var result = _studentManagementContext.SaveChanges();
+            if (result > 0)
+            {
+                return NoContent();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ StudentManagement/Entities/StudentManagementContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentManagement.Entities;
+
+public partial class StudentManagementContext : DbContext
+{
+    public StudentManagementContext()
+    {
+    }
+
+    public StudentManagementContext(DbContextOptions<StudentManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Room> Rooms { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Room>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Room__3214EC074AFD7ED1");
+
+            entity.ToTable("Room");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(256);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Student__3214EC07F8B802DF");
+
+            entity.ToTable("Student");
+
+            entity.HasIndex(e => e.Email, "UQ__Student__A9D10534CA38C729").IsUnique();
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Birthdate).HasColumnType("datetime");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.Name).HasMaxLength(256);
+
+            entity.HasOne(d => d.Room).WithMany(p => p.Students)
+                .HasForeignKey(d => d.RoomId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Student__RoomId__3C69FB99");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
